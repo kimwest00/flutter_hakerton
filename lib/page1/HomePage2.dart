23811febde1b70877eecb8a52cf2cd3a297ae04d
kimwest00 <kimwest00@hufs.ac.kr>
2022-02-05 02:57:04 +0900

    코드 중복부분 수정

--- lib/page1/HomePage2.dart
+++ lib/page1/HomePage2.dart
@@ -10,332 +10,7 @@ class HomePage2 extends StatefulWidget {
   const HomePage2({Key? key}) : super(key: key);
   @override
   _MyAppState createState() => _MyAppState();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        backgroundColor: mainColor,
-        title:Image.asset("assets/chario_logo.png",width:100,height:50),
-        //centerTitle: true,
-        actions: <Widget>[
-          IconButton(onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) =>HomePage1())
-            );},
-            icon: Icon(Icons.adb),),
-          IconButton(onPressed: (){
-            }, icon: Icon(Icons.account_balance))
-        ],
-      ),
-      body:ListView(
-        padding: const EdgeInsets.all(100),
-        children: <Widget>[
-          Container(
-            child:Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:<Widget> [
-                  Row(
-                    children:[
 
-                      ClipOval(
-                          clipper : MyClipper()
-                          ,child: Image.asset("assets/rufi.jpg",width: 50,height: 50,)
-                      ),
-                      Text("세이브더 칠드런",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                      SizedBox(width: 300.0,),
-                      Text("더 알아보기",style: TextStyle(fontSize: 15),),
-                      IconButton(onPressed: (){},icon: Icon(Icons.arrow_forward_ios),),
-
-                    ],
-                  ),
-                  SizedBox(height: 30.0,),
-                  Container(
-                    child: Row(
-                      children:[
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                      ],
-                    ),
-                  ),
-                ]
-            ),
-          ),
-          SizedBox(height: 30.0,),
-          Container(
-            child:Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:<Widget> [
-                  Row(
-                    children:[
-                      ClipOval(
-                          clipper : MyClipper()
-                          ,child: Image.asset("assets/rufi.jpg",width: 50,height: 50,)
-                      ),
-                      Text("유니세프",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                      SizedBox(width: 300.0,),
-                      Text("더 알아보기",style: TextStyle(fontSize: 15),),
-                      IconButton(onPressed: (){},icon: Icon(Icons.arrow_forward_ios),),
-
-                    ],
-                  ),
-                  SizedBox(height: 30.0,),
-                  Container(
-                    child: Row(
-                      children:[
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                      ],
-                    ),
-                  ),
-                ]
-            ),
-          ),
-          SizedBox(height: 30.0,),
-          Container(
-            child:Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:<Widget> [
-                  Row(
-                    children:[
-                      ClipOval(
-                          clipper : MyClipper()
-                          ,child: Image.asset("assets/rufi.jpg",width: 50,height: 50,)
-                      ),
-                      Text("유니세프",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                      SizedBox(width: 300.0,),
-                      Text("더 알아보기",style: TextStyle(fontSize: 15),),
-                      IconButton(onPressed: (){},icon: Icon(Icons.arrow_forward_ios),),
-
-                    ],
-                  ),
-                  SizedBox(height: 30.0,),
-                  Container(
-                    child: Row(
-                      children:[
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                      ],
-                    ),
-                  ),
-                ]
-            ),
-          ),
-          SizedBox(height: 30.0,),
-          Container(
-            child:Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:<Widget> [
-                  Row(
-                    children:[
-                      ClipOval(
-                          clipper : MyClipper()
-                          ,child: Image.asset("assets/rufi.jpg",width: 50,height: 50,)
-                      ),
-                      Text("유니세프",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                      SizedBox(width: 300.0,),
-                      Text("더 알아보기",style: TextStyle(fontSize: 15),),
-                      IconButton(onPressed: (){},icon: Icon(Icons.arrow_forward_ios),),
-
-                    ],
-                  ),
-                  SizedBox(height: 30.0,),
-                  Container(
-                    child: Row(
-                      children:[
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                      ],
-                    ),
-                  ),
-                ]
-            ),
-          ),
-          SizedBox(height: 30.0,),
-          Container(
-            child:Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:<Widget> [
-                  Row(
-                    children:[
-                      ClipOval(
-                          clipper : MyClipper()
-                          ,child: Image.asset("assets/rufi.jpg",width: 50,height: 50,)
-                      ),
-                      Text("유니세프",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                      SizedBox(width: 300.0,),
-                      Text("더 알아보기",style: TextStyle(fontSize: 15),),
-                      IconButton(onPressed: (){},icon: Icon(Icons.arrow_forward_ios),),
-
-                    ],
-                  ),
-                  SizedBox(height: 30.0,),
-                  Container(
-                    child: Row(
-                      children:[
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                      ],
-                    ),
-                  ),
-                ]
-            ),
-          ),
-          SizedBox(height: 30.0,),
-          Container(
-            child:Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:<Widget> [
-                  Row(
-                    children:[
-                      ClipOval(
-                          clipper : MyClipper()
-                          ,child: Image.asset("assets/rufi.jpg",width: 50,height: 50,)
-                      ),
-                      Text("유니세프",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                      SizedBox(width: 300.0,),
-                      Text("더 알아보기",style: TextStyle(fontSize: 15),),
-                      IconButton(onPressed: (){},icon: Icon(Icons.arrow_forward_ios),),
-
-                    ],
-                  ),
-                  SizedBox(height: 30.0,),
-                  Container(
-                    child: Row(
-                      children:[
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                        Column(
-                          children: [
-                            Image.asset("assets/rufi.jpg",width: 200,height: 200,),
-                            Text("이건 내 기사내용이다"),
-                          ],
-                        ),
-                        SizedBox(width: 30.0,),
-                      ],
-                    ),
-                  ),
-                ]
-            ),
-          ),
-          SizedBox(height: 30.0,),
-        ],
-      ),
-    );
-
-
-
-
-
-
-  }
 }
 class _MyAppState extends State<HomePage2>{
   late Future<Album> HomeAlbum;
@@ -347,17 +22,21 @@ class _MyAppState extends State<HomePage2>{
   }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Fetch Data Example',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
+    return Scaffold(
           appBar: AppBar(
-          title: const Text('Fetch Data Example'),
-    ),
+            backgroundColor: mainColor,
+            title:Image.asset("assets/chario_logo.png",width:100,height:50),
+            //centerTitle: true,
+            actions: <Widget>[
+              IconButton(onPressed: () {
+              },
+                icon: Icon(Icons.adb),),
+              IconButton(onPressed: (){
+                print("here");
+                }, icon: Icon(Icons.account_balance))
+            ],
+          ),
           body: ListView(
-
                   children:<Widget> [
                       FutureBuilder<Album>(
                         future: HomeAlbum,
@@ -610,9 +289,8 @@ class _MyAppState extends State<HomePage2>{
 
                   ]
               ),
-            ),
+            );
 
-        );
 
 
 
